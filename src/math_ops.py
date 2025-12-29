19e0dd80d0c2fbb0eded53ada87e7a8335df0635
Sourabh Patil <sourabhpatil777@gmail.com>
2025-12-29 16:39:36 +0530

    Added support for division

--- src/math_ops.py
+++ src/math_ops.py
@@ -3,3 +3,7 @@ def add(a,b):
 
 def sub(a,b):
     return a - b 
+
+def div(a, b):
+    assert b != 0
+    return a / b 
--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,4 +1,4 @@
-from src.math_ops import add, sub 
+from src.math_ops import add, sub, div
 
 def test_add():
     assert add(2,3) == 5 
@@ -8,3 +8,6 @@ def test_sub():
     assert sub(5,3) == 2 
     assert sub(-1,1) == -2 
 
+def test_div():
+    assert div(6,2) == 3 
+    assert div(0,10) == 0
